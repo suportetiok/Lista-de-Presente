e93c5eb3c27654301c4265259be57c557186fe42
suportetiok <suportetiok@gmail.com>
2026-06-01 21:15:26 -0400

    Update firebase.js
    CORRIGE O LOGIN COM EMAIL novamente

--- firebase.js
+++ firebase.js
@@ -1,32 +1,42 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getDatabase, ref, onValue, set, update, push, remove, get } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
-import { 
-  getAuth, 
-  signInWithEmailAndPassword, 
-  signInWithPopup, 
-  GoogleAuthProvider, 
-  signOut, 
-  onAuthStateChanged 
-} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
-// ✅ SUAS CONFIGURAÇÕES DO FIREBASE JÁ INSERIDAS AQUI
 const firebaseConfig = {
-  apiKey: "AIzaSyC7QQJ_c5wmd3GffLYDrQ3xG44LdXSApFg",
-  authDomain: "lista-de-presentes-33c7f.firebaseapp.com",
-  databaseURL: "https://lista-de-presentes-33c7f-default-rtdb.firebaseio.com",
-  projectId: "lista-de-presentes-33c7f",
-  storageBucket: "lista-de-presentes-33c7f.firebasestorage.app",
-  messagingSenderId: "540439045540",
-  appId: "1:540439045540:web:23587a2640c46784ab0d31"
+  // ⚠️ COLE AQUI SUAS CONFIGURAÇÕES DO FIREBASE (apiKey, authDomain, etc...)
+  apiKey: "SUA_API_KEY_AQUI",
+  authDomain: "SEU_DOMINIO_AQUI",
+  databaseURL: "SUA_URL_DO_BANCO_AQUI",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_SENDER_ID",
+  appId: "SEU_APP_ID"
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
 const providerGoogle = new GoogleAuthProvider();
 
 export { 
-  db, auth, providerGoogle, 
-  ref, onValue, set, update, push, remove, get, 
-  signInWithEmailAndPassword, signInWithPopup, signOut, onAuthStateChanged 
+  db, 
+  auth, 
+  providerGoogle, 
+  getDatabase,
+  getAuth,
+  signInWithEmailAndPassword, 
+  signInWithPopup, 
+  signOut, 
+  onAuthStateChanged,
+  ref, 
+  onValue, 
+  set, 
+  update, 
+  push, 
+  remove, 
+  get 
 };
+
+// Importações adicionais necessárias
+import { ref, onValue, set, update, push, remove, get } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
