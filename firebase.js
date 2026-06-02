98ecdd127180f71a3ac83eef7973c55356733475
suportetiok <suportetiok@gmail.com>
2026-06-01 20:33:33 -0400

    Update firebase.js
    Login por e-mail funcionando

--- firebase.js
+++ firebase.js
@@ -1,22 +1,23 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-import { getDatabase, ref, onValue, set, update, push, remove, get } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getDatabase, ref, onValue, set, update, push, remove, get } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 import { 
   getAuth, 
   signInWithEmailAndPassword, 
-  signInWithPopup,       
-  GoogleAuthProvider,    
+  signInWithPopup, 
+  GoogleAuthProvider, 
   signOut, 
   onAuthStateChanged 
-} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
+// ✅ SUAS CONFIGURAÇÕES DO FIREBASE JÁ INSERIDAS AQUI
 const firebaseConfig = {
-    apiKey: "AIzaSyC7QQJ_c5wmd3GffLYDrQ3xG44LdXSApFg",
-    authDomain: "lista-de-presentes-33c7f.firebaseapp.com",
-    databaseURL: "https://lista-de-presentes-33c7f-default-rtdb.firebaseio.com",
-    projectId: "lista-de-presentes-33c7f",
-    storageBucket: "lista-de-presentes-33c7f.firebasestorage.app",
-    messagingSenderId: "540439045540",
-    appId: "1:540439045540:web:23587a2640c46784ab0d31"
+  apiKey: "AIzaSyC7QQJ_c5wmd3GffLYDrQ3xG44LdXSApFg",
+  authDomain: "lista-de-presentes-33c7f.firebaseapp.com",
+  databaseURL: "https://lista-de-presentes-33c7f-default-rtdb.firebaseio.com",
+  projectId: "lista-de-presentes-33c7f",
+  storageBucket: "lista-de-presentes-33c7f.firebasestorage.app",
+  messagingSenderId: "540439045540",
+  appId: "1:540439045540:web:23587a2640c46784ab0d31"
 };
 
 const app = initializeApp(firebaseConfig);
