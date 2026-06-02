25f4bfb6a7b645d6cf2eb204644fd6328ef6cea6
suportetiok <suportetiok@gmail.com>
2026-06-01 21:17:36 -0400

    Update firebase.js
    sem
    www.gstatic.com/firebasejs

--- firebase.js
+++ firebase.js
@@ -1,16 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
-import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  // ⚠️ COLE AQUI SUAS CONFIGURAÇÕES DO FIREBASE (apiKey, authDomain, etc...)
-  apiKey: "SUA_API_KEY_AQUI",
-  authDomain: "SEU_DOMINIO_AQUI",
-  databaseURL: "SUA_URL_DO_BANCO_AQUI",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_STORAGE_BUCKET",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyC7QQJ_c5wmd3GffLYDrQ3xG44LdXSApFg",
+  authDomain: "lista-de-presentes-33c7f.firebaseapp.com",
+  databaseURL: "https://lista-de-presentes-33c7f-default-rtdb.firebaseio.com",
+  projectId: "lista-de-presentes-33c7f",
+  storageBucket: "lista-de-presentes-33c7f.firebasestorage.app",
+  messagingSenderId: "540439045540",
+  appId: "1:540439045540:web:23587a2640c46784ab0d31"
 };
 
 // Inicializa o Firebase
