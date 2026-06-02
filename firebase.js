055aefa2f4959e73c105cfb2982a424c1d4f83ec
suportetiok <suportetiok@gmail.com>
2026-06-02 00:14:47 -0400

    subir tudo d novo

--- firebase.js
+++ firebase.js
@@ -1,31 +1,42 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-import { getDatabase, ref, onValue, set, update, push, remove, get } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
-import { 
-  getAuth, 
-  signInWithEmailAndPassword, 
-  signInWithPopup,       
-  GoogleAuthProvider,    
-  signOut, 
-  onAuthStateChanged 
-} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC7QQJ_c5wmd3GffLYDrQ3xG44LdXSApFg",
-    authDomain: "lista-de-presentes-33c7f.firebaseapp.com",
-    databaseURL: "https://lista-de-presentes-33c7f-default-rtdb.firebaseio.com",
-    projectId: "lista-de-presentes-33c7f",
-    storageBucket: "lista-de-presentes-33c7f.firebasestorage.app",
-    messagingSenderId: "540439045540",
-    appId: "1:540439045540:web:23587a2640c46784ab0d31"
+  apiKey: "AIzaSyC7QQJ_c5wmd3GffLYDrQ3xG44LdXSApFg",
+  authDomain: "lista-de-presentes-33c7f.firebaseapp.com",
+  databaseURL: "https://lista-de-presentes-33c7f-default-rtdb.firebaseio.com",
+  projectId: "lista-de-presentes-33c7f",
+  storageBucket: "lista-de-presentes-33c7f.firebasestorage.app",
+  messagingSenderId: "540439045540",
+  appId: "1:540439045540:web:23587a2640c46784ab0d31"
 };
 
+
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 const auth = getAuth(app);
 const providerGoogle = new GoogleAuthProvider();
 
 export { 
-  db, auth, providerGoogle, 
-  ref, onValue, set, update, push, remove, get, 
-  signInWithEmailAndPassword, signInWithPopup, signOut, onAuthStateChanged 
+  db, 
+  auth, 
+  providerGoogle, 
+  getDatabase,
+  getAuth,
+  signInWithEmailAndPassword, 
+  signInWithPopup, 
+  signOut, 
+  onAuthStateChanged,
+  ref, 
+  onValue, 
+  set, 
+  update, 
+  push, 
+  remove, 
+  get 
 };
+
+// Importações adicionais necessárias
+import { ref, onValue, set, update, push, remove, get } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
