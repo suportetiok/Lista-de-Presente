57767964a0d5ad61d45f7df54fa92966239d9697
suportetiok <suportetiok@gmail.com>
2026-06-07 12:52:34 -0400

    Update firebase.js
    reserva2

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-// Importa as funções necessárias do SDK do Firebase
+   // Importa as funções necessárias do SDK do Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
 import { 
     getDatabase, 
@@ -20,6 +20,7 @@ import {
     onAuthStateChanged 
 } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
 
+// ✅ SUAS CONFIGURAÇÕES DO FIREBASE (COLE AQUI AS SUAS!)
 const firebaseConfig = {
   apiKey: "AIzaSyC7QQJ_c5wmd3GffLYDrQ3xG44LdXSApFg",
   authDomain: "lista-de-presentes-33c7f.firebaseapp.com",
@@ -28,7 +29,7 @@ const firebaseConfig = {
   storageBucket: "lista-de-presentes-33c7f.firebasestorage.app",
   messagingSenderId: "540439045540",
   appId: "1:540439045540:web:23587a2640c46784ab0d31"
-};
+
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
