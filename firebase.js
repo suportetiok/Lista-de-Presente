4faa546af9dc5782b8bb34a659f28764edefb52c
suportetiok <suportetiok@gmail.com>
2026-06-07 13:03:11 -0400

    volta fire

--- firebase.js
+++ firebase.js
@@ -1,26 +1,7 @@
-   // Importa as funções necessárias do SDK do Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-import { 
-    getDatabase, 
-    ref, 
-    onValue, 
-    set, 
-    update, 
-    push, 
-    remove, 
-    get, 
-    child 
-} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-database.js";
-import { 
-    getAuth, 
-    signInWithEmailAndPassword, 
-    signInWithPopup, 
-    GoogleAuthProvider, 
-    signOut, 
-    onAuthStateChanged 
-} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
+import { getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 
-// ✅ SUAS CONFIGURAÇÕES DO FIREBASE (COLE AQUI AS SUAS!)
 const firebaseConfig = {
   apiKey: "AIzaSyC7QQJ_c5wmd3GffLYDrQ3xG44LdXSApFg",
   authDomain: "lista-de-presentes-33c7f.firebaseapp.com",
@@ -29,30 +10,33 @@ const firebaseConfig = {
   storageBucket: "lista-de-presentes-33c7f.firebasestorage.app",
   messagingSenderId: "540439045540",
   appId: "1:540439045540:web:23587a2640c46784ab0d31"
+};
+
 
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-
-// Inicializa os serviços que usamos
 const db = getDatabase(app);
 const auth = getAuth(app);
 const providerGoogle = new GoogleAuthProvider();
 
-// ✅ EXPORTA TUDO O QUE O app.js PRECISA (Isso era o que estava faltando ou errado antes!)
 export { 
-    db, 
-    auth, 
-    providerGoogle, 
-    ref, 
-    onValue, 
-    set, 
-    update, 
-    push, 
-    remove, 
-    get, 
-    child,
-    signInWithEmailAndPassword,
-    signInWithPopup,
-    signOut,
-    onAuthStateChanged
+  db, 
+  auth, 
+  providerGoogle, 
+  getDatabase,
+  getAuth,
+  signInWithEmailAndPassword, 
+  signInWithPopup, 
+  signOut, 
+  onAuthStateChanged,
+  ref, 
+  onValue, 
+  set, 
+  update, 
+  push, 
+  remove, 
+  get 
 };
+
+// Importações adicionais necessárias
+import { ref, onValue, set, update, push, remove, get } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
